2a78816458d77a2808ec8d407585a677a898f3cb
dong.wang <dong.wang@yingzt.com>
2019-04-26 10:17:25 +0800

    去掉描述

--- TestCategroy/main.m
+++ TestCategroy/main.m
@@ -1,11 +1,3 @@
-//
-//  *@项目名称:  TestCategroy
-//  *@文件名称:  main.m
-//  *@Date 2019/2/11
-//  *@Author wangzelong 
-//  *@Copyright © :  2014-2019 X-Financial Inc.   All rights reserved.
-//  *注意：本内容仅限于小赢科技有限责任公内部传阅，禁止外泄以及用于其他的商业目的。
-//
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
