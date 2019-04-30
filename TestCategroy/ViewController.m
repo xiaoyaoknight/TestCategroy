c5957d26ced14c2b4d46658cbd06fc913a0677bb
dong.wang <dong.wang@yingzt.com>
2019-04-30 16:36:21 +0800

    更新category

--- TestCategroy/ViewController.m
+++ TestCategroy/ViewController.m
@@ -21,9 +21,23 @@
 
 @implementation ViewController
 
+
+/*
+ 分类底层结构 查看Person+Test1.cpp文件
+ 
+ typedef struct category_t {
+ const char *name;
+ classref_t cls;
+ struct method_list_t *instanceMethods;
+ struct method_list_t *classMethods;
+ struct protocol_list_t *protocols;
+ struct property_list_t *instanceProperties;
+ } category_t;
+ */
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
     
     NSLog(@"---------测试load方法Class和Category加载顺序-------------");
     /*
@@ -39,13 +53,16 @@
     
     NSLog(@"---------测试initialize方法Class和Category加载顺序-------------");
     /*
-     通过源码可以看到, 当一个类在查找方法的时候, 会先判断当前类是否初始化, 如果没有初始化就会去掉用initialize方法
-     如果这个类的父类没有初始化, 就会先调用父类的initialize方法, 再调用自己的initialize方法
-     类在调用initialize时, 使用的是objc_msgSend消息机制调用
+     通过源码可以看到,
+     +initialize方法会在类第一次接收到消息时调用 使用的是objc_msgSend消息机制调用
+     先调用父类的+initialize，再调用子类的+initialize
+     (先初始化父类，再初始化子类，每个类只会初始化1次)
      */
     
     //    [Person alloc];
     
+    // 如果子类没有实现+initialize，会调用父类的+initialize（所以父类的+initialize可能会被调用多次）
+    // 如果分类实现了+initialize，就覆盖类本身的+initialize调用
     // (student中去掉initialize进行打印，初始化的时候发现父类没有初始化，之后才会初始化子类，可能调用2次initialize，后面打印其实是student)
     [Student alloc];
     
